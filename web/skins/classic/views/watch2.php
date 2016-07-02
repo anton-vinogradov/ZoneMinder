0a6fb8842244c239fbbd309db0c2119d0adb5a5b
Anton <av@apache.org>
2016-07-02 14:22:10 +0300

    my improvements

--- web/skins/classic/views/watch2.php
+++ web/skins/classic/views/watch2.php
@@ -77,26 +77,32 @@ xhtmlHeaders( __FILE__, $monitor->Name()." - ".translate('Feed') );
 <body>
 <div id="pageBlack"
      style="margin:auto; position:absolute; z-index: 1; background:#000000; width:2000px; height:2000px"></div>
-<div id="page0" style="margin:auto; position:absolute; top: 20px; z-index: 20; left: 50%; margin-left: +350px;">
-    <a href="index.php?view=watch2&amp;mid=1" style="font-size: 200px; color:#222222;">*</a>
+<div id="page0" style="margin:auto; position:absolute; top: 0px; z-index: 20; left: 50%; margin-left: +350px;">
+    <a href="index.php?view=watch2&amp;mid=1" style="font-size: 150px; color:#222222;">&#8635;</a>
 </div>
-<div id="page1" style="margin:auto; position:absolute; top: 20px; z-index: 20; left: 50%; margin-left: -150px;">
-    <a href="index.php?view=watch3&amp;mid=1" style="font-size: 200px; color:#000000;">*</a>
+<div id="page1" style="margin:auto; position:absolute; top: 0px; z-index: 20; left: 50%; margin-left: -150px;">
+    <a href="index.php?view=watch3&amp;mid=1" style="font-size: 150px; color:#000000;">&#10006;</a>
 </div>
 <div id="page" style="margin:auto; position:absolute; top: -265px; left: -150px; z-index: 2;">
     <?php outputImageStream("liveStream", $streamSrc, reScale($monitor->Width(), 175), reScale($monitor->Height(), 140), $monitor->Name()); ?>
 </div>
-<div id="page" style="margin:auto; position:absolute; top: 0px; left: 0px; z-index: 18;">
-    <table cellpadding=0 cellspacing=0 width=300
-           style="border:solid 1px #000000;font-family:Arial;font-size:18px;background-color:#000000">
-        <tr>
-            <td valign=top style="padding:0;">
-                <iframe
-                    src="http://rp5.ru/htmla.php?id=7285&lang=ru&um=00001&bg=%23000000&ft=%23ffffff&fc=%23000000&c=%23ffffff&f=Arial&s=24&sc=4"
-                    width=100% height=500 frameborder=0 scrolling=no style="margin:0;"></iframe>
-            </td>
-        </tr>
-    </table>
+<!--<div id="page3" style="margin:auto; position:absolute; top: 0px; left: 0px; z-index: 18;">-->
+<!--    <table cellpadding=0 cellspacing=0 width=300-->
+<!--           style="border:solid 1px #000000;font-family:Arial;font-size:18px;background-color:#000000">-->
+<!--        <tr>-->
+<!--            <td valign=top style="padding:0;">-->
+<!--                <iframe-->
+<!--                    src="http://rp5.ru/htmla.php?id=7285&lang=ru&um=00001&bg=%23000000&ft=%23ffffff&fc=%23000000&c=%23ffffff&f=Arial&s=24&sc=4"-->
+<!--                    width=100% height=500 frameborder=0 scrolling=no style="margin:0;"></iframe>-->
+<!--            </td>-->
+<!--        </tr>-->
+<!--    </table>-->
+<!--</div>-->
+<div id="page4" style="margin:auto; position:absolute; top: 0px; left: 0px; z-index: 18; overflow: hidden">
+    <iframe id="optomaFeed"
+            src="http://www.rp5.ru/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3%D0%B5"
+            scrolling="no"
+            frameborder="0" style="position: relative; left: -155px; top: -500px;" height="770px" width="480px"></iframe>
 </div>
 </body>
 </html>
