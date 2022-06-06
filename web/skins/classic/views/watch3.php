a1d5461dfb7474c0d554b7e28ac520e114b9126a
Anton Vinogradov <av@apache.org>
2022-06-06 13:19:47 +0300

    wip

--- web/skins/classic/views/watch3.php
+++ web/skins/classic/views/watch3.php
@@ -1,5 +1,5 @@
 <html>
-<body style="background:#000000; href=" index.php?view=watch2&amp;mid=1">
+<body style="background:#000000; href=" index.php?view=watch2&amp;mid=3">
 <div>
 	<a href="index.php?view=watch2&amp;mid=3"
 	   style="font-size: 240px; top: -300px; color:#111111; z-index: 20;">push*me<br>push*me</a>
